0af060a7fe5e0d0fdf78bf34724ce60fde60b6fa
AnnEva187 <mort562@mail.ru>
2026-05-03 02:14:21 +0700

    обновлены Python-файлы

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,3 +1,3 @@
 first_name = input("Введите ваше имя:")
 last_name = input("Введите вашу фамилию:")
-print(f"меня зовут: {first_name} {last_name}")
+print(f"Вас зовут: {last_name} {first_name}")
--- lesson_01/lesson_1_task_4.py
+++ lesson_01/lesson_1_task_4.py
@@ -1,2 +1,5 @@
 def print_greeting():
     print("Привет, мир!")
+
+
+print_greeting()
